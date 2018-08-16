a4a686b4c0ad26a917aef7feaf5afa69f46e48a7
danmj <daniel.jung@stern.nyu.edu>
2018-08-16 15:21:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,3 @@
 # Write your code here.
 
-namelist = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-def badge_maker(name)
-  puts "Hello, my name is #{name}"
-end
-
-def batch_badge_creator(namelist)
-  namelist.collect do |name|
-    badge_maker(name)
-  end
-end
-
-def assign_rooms
-  room=0
-  namelist.collect do |name|
-  room+=1
-  "Hello, #{name}! You'll be assigned to room #{room}!"
-  end
-end
 
